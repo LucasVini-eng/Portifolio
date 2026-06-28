50d8b05c17ffdb888c952db80f9fee8bdcdbb3a7
Lucas Vinicius <vinidev.eng@gmail.com>
2026-06-28 14:48:10 -0300

    Delete js/script_alert_dados.js

--- js/script_alert_dados.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-      const popup = document.getElementById('popup');
-      popup.style.display = 'block';
-    });
-
-    function fecharPopup() {
-      document.getElementById('popup').style.display = 'none';
-    }
